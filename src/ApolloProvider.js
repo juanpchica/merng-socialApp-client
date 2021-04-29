a9f237f06dde27d129c5266ffae7bfa4e53084c1
juanpchica <juanpchica@hotmail.com>
2021-04-29 14:24:15 -0500

    Uri apollo client changed

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,7 +5,7 @@ import { setContext } from "@apollo/client/link/context";
 import { ApolloProvider } from "@apollo/client/react";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://merng-server-jpc.herokuapp.com/",
 });
 
 const authLink = setContext((request, previousContext) => {
